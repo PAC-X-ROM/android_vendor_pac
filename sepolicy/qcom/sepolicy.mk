ac3db05dca3ca183533b73d7f2c882e2e6edafc2
Ram9199 <sriramscience7@gmail.com>
2019-01-22 21:51:19 +0530

    Need to remake things for 9.0
    this is a bump from 6.0 to 9.9
    
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

--- sepolicy/qcom/sepolicy.mk
+++
@@ -1,2 +0,0 @@
-BOARD_SEPOLICY_DIRS += \
-    vendor/pac/sepolicy/qcom
--- sepolicy/sepolicy.mk
+++
@@ -1,7 +0,0 @@
-#
-# This policy configuration will be used by all products that
-# inherit from PAC
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/pac/sepolicy
